af05a49bff4378600327622d068faabf5c858d8a
robertbutacu <robert.butacu@yahoo.com>
2018-02-24 00:21:55 +0200

    pseudocode

--- src/main/scala/first/lab/StrassenAlgorithm.scala
+++ src/main/scala/first/lab/StrassenAlgorithm.scala
@@ -7,6 +7,16 @@ object StrassenAlgorithm {
                           secondMatrix: StrassenMatrix[A],
                           n: Int,
                           minN: Int): StrassenMatrix[A] = {
+    /*
+    if( n <= minN )
+      compute P1, P2, ETC ETC
+    else
+      composeMatrices(strassenAlgorithm(firstMatrix.A11, secondMatrix.B11, n / 2, minN),
+                      strassenAlgorithm(firstMatrix.A12, secondMatrix.B12, n / 2, minN),
+                      strassenAlgorithm(firstMatrix.A21, secondMatrix.B21, n / 2, minN),
+                      strassenAlgorithm(firstMatrix.A22, secondMatrix.B22, n / 2, minN)
+                     )
+     */
     new StrassenMatrix[A](List.empty)
   }
 }
