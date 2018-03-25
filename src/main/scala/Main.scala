b6e03be707d5fd19afdfce8c6c36eadf08873cd0
robertbutacu <robert.butacu@yahoo.com>
2018-03-25 21:38:31 +0300

    first not associative multiplication

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,5 +2,15 @@ import first.lab.{MachinePrecision, StrassenAlgorithm}
 import first.lab.structures.StrassenMatrix
 
 object Main extends App {
+  lazy val m1 = StrassenMatrix(
+    List(List(-1.5, -1.5, -1.5, -1.5, -1.5),
+      List(-1.5, -1.5, -1.5, -1.5, -1.5),
+      List(-1.5, -1.5, -1.5, -1.5, -1.5),
+      List(-1.5, -1.5, -1.5, -1.5, -1.5),
+      List(-1.5, -1.5, -1.5, -1.5, -1.5)
+    )
+  )
+
+  MachinePrecision.firstMulNotAss.foreach(println)
 
 }
--- src/main/scala/first/lab/MachinePrecision.scala
+++ src/main/scala/first/lab/MachinePrecision.scala
@@ -6,9 +6,21 @@ object MachinePrecision {
     Stream.cons(Math.pow(10, -start), negativePowsOfTen(start + 1))
   }
 
+  private def nrStream(start: Double): Stream[Double] = {
+    Stream.cons(start, nrStream(start + 1.0))
+  }
+
+
   val machinePrecision: Double =
     negativePowsOfTen(0).view.takeWhile(_ + 1.0 != 1).last
 
   def isAdditionNotAssociativeWithMachinePrecision: Boolean =
     ((1.0 + machinePrecision) + machinePrecision) != (1.0 + (machinePrecision + machinePrecision))
+
+  def firstMulNotAss: List[Double] =
+    nrStream(0.0)
+      .dropWhile(x => x * (machinePrecision * machinePrecision) == (x * machinePrecision) * machinePrecision)
+      .take(1000)
+      .filter(x => x * (machinePrecision * machinePrecision) != (x * machinePrecision) * machinePrecision)
+      .toList
 }
